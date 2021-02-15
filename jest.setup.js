156e59b397dfd5f45ae3b3258f37b98a93b1f4c2
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-02-15 11:33:09 +0100

    adding testing library

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,6 @@
+import '@testing-library/jest-dom/extend-expect';
 import next from 'next';
+
 const app = next({ dev: true });
 
 afterAll(async (done) => {
---
+++ test/pages/index.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../../pages/index';
+
+test('renders deploy link', () => {
+  const { getByText } = render(<Home />);
+  const element = getByText(/Luis Cipriani and this is a collection/);
+  expect(element).toBeInTheDocument();
+});
