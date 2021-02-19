f6b29f8bfebeb4d72838170e60fb88fdd8326f94
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-02-19 17:50:48 +0100

    Adding test to Date component

---
+++ test/components/date.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Date from '../../components/date';
+
+test('renders article date in the right format', () => {
+  render(<Date dateString="2017-02-07T00:00:00.000Z" />);
+  const date = screen.getByText('2017-02-07');
+  expect(date).toBeInTheDocument();
+});
