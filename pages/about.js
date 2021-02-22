bfa1e4fe848770243dda0a3bcc7c16e8bcf9a7c4
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-02-22 15:14:47 +0100

    adding about page

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import styles from '../styles/About.module.css';
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>About - {process.env.NEXT_PUBLIC_SITE_TITLE}</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="Luis Cipriani" />
+        <meta name="og:title" content={`About - ${process.env.NEXT_PUBLIC_SITE_TITLE}`} />
+        <meta property="og:image" content="" />
+      </Head>
+      <article>
+        <header className={styles.articleHeader}>
+          <h1 className={styles.title}>About me</h1>
+        </header>
+        <section className="marquidaoum">
+          <p>Hello world</p>
+        </section>
+      </article>
+    </Layout>
+  );
+}
