3dce70501f10ade59ccf046496b8c68024ef793b
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-03-08 13:18:06 +0100

    fixing some perf issues

--- components/Language.js
+++ components/Language.js
@@ -5,7 +5,8 @@ export default function Language({ lang }) {
   };
   return (
     <>
-      <img src={`/locales/${lang}.png`} alt="pt-BR flag" /> <em>Content in {languages[lang]}.</em>{' '}
+      <img src={`/locales/${lang}.png`} alt="pt-BR flag" width="16" height="12" />{' '}
+      <em>Content in {languages[lang]}.</em>{' '}
       <style jsx>{`
         img {
           display: inline;
