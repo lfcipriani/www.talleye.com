28f7d0a89db33bab87145da5aa8128eb4acc073e
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-03-08 17:03:04 +0100

    testing another analytics

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,20 @@ class MyDocument extends Document {
             data-domain="talleye.com"
             src="https://stats.talleye.com/js/index.js"
           ></script>
+          <script>
+            if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!==
+            0)
+            {fetch(
+              'https://counter.dev/track?' +
+                new URLSearchParams({
+                  referrer: document.referrer,
+                  screen: screen.width + 'x' + screen.height,
+                  user: 'yrZVUWGdxFeQGcsVjpTm',
+                  utcoffset: '1',
+                })
+            )}
+            ;sessionStorage.setItem("_swa","1");
+          </script>
         </Head>
         <body>
           <Main />
