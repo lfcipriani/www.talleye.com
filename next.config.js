841f831e82acea13bae77c3f8f7ec51e8eaddbc6
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-03-24 18:31:28 +0100

    configuring redirections from old tumblr blog

--- next.config.js
+++ next.config.js
@@ -4,4 +4,49 @@ module.exports = {
     defaultLocale: 'en',
     localeDetection: false,
   },
+  redirects: async () => {
+    // redirecting from lfcipriani.tumblr.com
+    return [
+      {
+        source: '/post/65952280899',
+        destination: '/posts/the-4-basic-flows-of-http-caching',
+        permanent: true,
+      },
+      {
+        source: '/post/62413346456',
+        destination: '/posts/an-easy-way-to-build-autocomplete-search-for-api-docs',
+        permanent: true,
+      },
+      {
+        source: '/post/42035650273',
+        destination: '/posts/alexandria-platform',
+        permanent: true,
+      },
+      {
+        source: '/post/24200885922',
+        destination: '/posts/explaining-semantic-web',
+        permanent: true,
+      },
+      {
+        source: '/post/21380734930',
+        destination: '/pt-BR/posts/extracao-de-dados-publicos-com-yql-yahoo-pipes-1',
+        permanent: true,
+      },
+      {
+        source: '/post/21381029425',
+        destination: '/pt-BR/posts/extracao-de-dados-publicos-com-yql-yahoo-pipes-2',
+        permanent: true,
+      },
+      {
+        source: '/post/21364056907',
+        destination: '/pt-BR/posts/brincando-com-apis-de-xmpp',
+        permanent: true,
+      },
+      {
+        source: '/post/21361278423',
+        destination: '/pt-BR/posts/breve-introducao-ao-xmpp',
+        permanent: true,
+      },
+    ];
+  },
 };
