aa85ccad831abd83baf7bfceca625d6f16d17eba
Luis Cipriani <37157+lfcipriani@users.noreply.github.com>
2021-05-30 12:50:37 +0200

    ncu dependency bump

--- jest.config.js
+++ jest.config.js
@@ -37,4 +37,6 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+
+  testEnvironment: 'jsdom',
 };
